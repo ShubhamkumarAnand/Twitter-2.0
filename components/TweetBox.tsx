583389f3e85c15ef65d3a8679c8c441015074030
imskanand <the.anand3099@gmail.com>
2022-05-18 22:39:36 +0530

    Taking Input using the state and disabling the tweet button when there is no text

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -5,31 +5,38 @@ import {
   PhotographIcon,
   SearchCircleIcon,
 } from '@heroicons/react/outline'
+import { useState } from 'react'
 
 function TweetBox() {
+  const [input, setInput] = useState<string>('')
   return (
-    <div className="flex space-x-2 p-5">
+    <div className="flex p-5 space-x-2">
       <img
         src="https://links.papareact.com/gll"
         alt=""
-        className="mt4 h-14 w-14 rounded-full object-cover"
+        className="object-cover rounded-full mt4 h-14 w-14"
       />
-      <div className="flex flex-1 items-center pl-2">
-        <form className="flex flex-1 flex-col">
+      <div className="flex items-center flex-1 pl-2">
+        <form className="flex flex-col flex-1">
           <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             type="text"
             placeholder="What is Happening?"
-            className="h-24 w-full text-xl outline-none placeholder:text-xl"
+            className="w-full h-24 text-xl outline-none placeholder:text-xl"
           />
           <div className="flex items-center">
             <div className="flex flex-1 space-x-2 text-twitter">
-              <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-200 ease-out hover:scale-150" />
-              <SearchCircleIcon className="h-5 w-5 cursor-pointer transition-transform duration-200 ease-out hover:scale-150" />
-              <EmojiHappyIcon className="h-5 w-5 cursor-pointer transition-transform duration-200 ease-out hover:scale-150" />
-              <CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-200 ease-out hover:scale-150" />
-              <LocationMarkerIcon className="h-5 w-5 cursor-pointer transition-transform duration-200 ease-out hover:scale-150" />
+              <PhotographIcon className="w-5 h-5 transition-transform duration-200 ease-out cursor-pointer hover:scale-150" />
+              <SearchCircleIcon className="w-5 h-5 transition-transform duration-200 ease-out cursor-pointer hover:scale-150" />
+              <EmojiHappyIcon className="w-5 h-5 transition-transform duration-200 ease-out cursor-pointer hover:scale-150" />
+              <CalendarIcon className="w-5 h-5 transition-transform duration-200 ease-out cursor-pointer hover:scale-150" />
+              <LocationMarkerIcon className="w-5 h-5 transition-transform duration-200 ease-out cursor-pointer hover:scale-150" />
             </div>
-            <button className="rounded-full bg-twitter px-5 py-2 font-bold text-white">
+            <button
+              disabled={!input}
+              className="px-5 py-2 font-bold text-white rounded-full bg-twitter disabled:bg-gray-400"
+            >
               Tweet
             </button>
           </div>
